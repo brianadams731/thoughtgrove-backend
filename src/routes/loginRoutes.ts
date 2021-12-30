309adcb2bca9701f99172ddabaf063869d808b18
brianadams731 <brianadams731@gmail.com>
2021-12-29 22:12:17 -0800

    Added ID into login query

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -8,13 +8,15 @@ loginRouter.route("/login")
     .post(async(req,res) =>{
         const loginCred = req.body;
         const user = await User.findOne({
-            select:["email","password"],
+            select:["email","password","id"],
             where:{email:loginCred.email}
         });
+        // Check for user ahead to save hashing against an invalid user
         if(!user){
             return res.status(401).send("Error: Invalid Credentials")
         
         }
+
         const isValidPassword = await checkHashedPasswordAsync(loginCred.password, user.password);
         if(!isValidPassword){
             return res.status(401).send("Error: Invalid Credentials")
