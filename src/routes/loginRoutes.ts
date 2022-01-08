a5a69d9390b03cb3c0a5a59691bba49e17fbb698
brianadams731 <brianadams731@gmail.com>
2022-01-07 17:38:55 -0800

    Error thrown when console.log is removed in login route

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -7,8 +7,13 @@ const loginRouter = express.Router();
 loginRouter.route("/login")
     .post(async(req,res) =>{
         if(req.session.userID){
-            return res.status(500).send("Error: User Already Loged In");
+            req.session.destroy((err)=>{
+                if(err){
+                    return res.status(500).send("Error: Cannot log existing user out");
+                }
+            });
         }
+
         const loginCred = req.body;
         const user = await User.findOne({
             select:["email","password","id"],
@@ -17,16 +22,16 @@ loginRouter.route("/login")
         // Checking for user ahead to save hashing against an invalid user
         if(!user){
             return res.status(401).send("Error: Invalid Credentials")
-        
         }
 
         const isValidPassword = await checkHashedPasswordAsync(loginCred.password, user.password);
+        
         if(!isValidPassword){
             return res.status(401).send("Error: Invalid Credentials")
         }else{
+            console.log(user);
             req.session.userID = user.id;
-            // TODO set correct redirect
-            return res.json(user);
+            return res.status(200).send("Good");
         }
     })
 
